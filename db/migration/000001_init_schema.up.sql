55367792782ed2782e943bbce837137f2b9e25fd
Joy Sarkar <joysarkar8171@gmail.com>
2023-11-03 15:03:05 +0530

    migrate up and migrate down sql added

---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,38 @@
+CREATE TABLE "accout" (
+  "account_id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" double precision NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transactions" (
+  "transaction_id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" double precision NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "accout" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transactions" ("from_account_id");
+
+CREATE INDEX ON "transactions" ("to_account_id");
+
+CREATE INDEX ON "transactions" ("from_account_id", "to_account_id");
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accout" ("account_id");
+
+ALTER TABLE "transactions" ADD FOREIGN KEY ("from_account_id") REFERENCES "accout" ("account_id");
+
+ALTER TABLE "transactions" ADD FOREIGN KEY ("to_account_id") REFERENCES "accout" ("account_id");
